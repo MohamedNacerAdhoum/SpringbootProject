84b6c3ea4175f436a948afbbee04d983f25bc59d
Jotakea <naceradhoum@gmail.com>
2024-10-14 21:38:38 +0100

    keyword-JPQL

--- src/main/java/com/esprit/tic/twin/springproject/repositories/ChambreRepository.java
+++ src/main/java/com/esprit/tic/twin/springproject/repositories/ChambreRepository.java
@@ -9,11 +9,19 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ChambreRepository extends JpaRepository<Chambre, Long> {
+
+    //Keyword
+
     List<Chambre> findByBlocNomBlocAndTypeC(String nomBloc, String type);
+    List<Chambre> findByReservationsestValide(Boolean status);
+    List<Chambre> findByBlocNomBlocAndCapaciteBlocGreaterThan(String nomBloc, long capacite);
+
+    //JPQL
+
     @Query("SELECT e FROM Chambre e WHERE e.bloc.nomBloc=:nomBloc AND e.typeC=:type")
     List<Chambre> retrieveChambreDoubleByBloc(@Param("nomBloc") String nomBloc, @Param("type") String type);
-    List<Chambre> findByReservationsestValide(Boolean status);
     @Query("SELECT e FROM Chambre e JOIN e.reservations f WHERE f.estValide=:status")
     List<Chambre> retrieveChambreByReservation(@Param("status")Boolean status);
-
+    @Query("SELECT e FROM Chambre e WHERE e.bloc.nomBloc=:nomBloc AND e.bloc.capaciteBloc>:capacite")
+    List<Chambre> retrieveChambreDoubleByBlocAndCapacite(@Param("nomBloc") String nomBloc, @Param("capacite") long capacite);
 }
